34ffe78f2546bb5c1382e7cb47f935389c85c1a2
albertibus <albertopilladocorreo@gmail.com>
2025-03-22 18:31:23 +0100

    [ADD] sql scripts to create the dwh and set up the schemas plus python script to automate the process

---
+++ scripts/sql/set_up_datawarehouse.sql
@@ -0,0 +1,11 @@
+/*
+=============================================================
+Create Schemas
+=============================================================
+Script Purpose:
+    Set up the data warehouse:
+        -The script sets up three schemas within the database: 'bronze', 'silver', and 'gold'.*/
+
+CREATE SCHEMA bronze;
+CREATE SCHEMA silver;
+CREATE SCHEMA gold;
